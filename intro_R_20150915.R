0b35a7cfe0aaaf794bdce7cb61cb40b6e7632cc9
Genevieve Davis <genevieve.davis@noaa.gov>
2015-09-15 09:36:05 -0400

    initial commit

---
+++ intro_R_20150915.R
@@ -0,0 +1,35 @@
+# Calculator
+2 + 2
+
+# Order of operations
+3 + 4 * (10 - 5)^2
+
+# Scientific notation
+.0003
+3 * 10^(-4)
+3e-4
+
+# Mathematical functions
+log (2)    # natural log
+log10(2)   # base-10 log
+exp(.5)    # e~(1/2)
+
+# Comparisons
+# Operators  (R Operators = "and" "or" etc)
+
+# equal to
+2 == 2
+3 == 2
+
+# not equal to 
+2 != 2
+2 != 3
+
+#greater/less than
+2 < 3
+2 > 3
+
+2 < 2
+2 <= 2
+2 <= 3
+
